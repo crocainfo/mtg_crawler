9471f9759016d590787b2ecd4de0083ec637b79e
carovi0696 <yabsurdo2op>
2021-05-02 18:54:24 +0200

    Got first links and expansions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mtg_crawler
 Crawler for major Magic The Gathering cardshops
+
+
+Check if card exists, filter by foil alternative art, i repeat keep the lowest price
+need to download big and small card images
---
+++ cardkingdom/cardkingdom.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false }); // default is true
+    // const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    let linksSelector = '.row.anchorList table tbody tr td:first-child a';
+    await page.goto('https://www.cardkingdom.com/catalog/magic_the_gathering/by_az',  {waitUntil: 'networkidle2'});
+    await page.waitForSelector(linksSelector);
+
+    // Long way to do it
+    // let links = await page.evaluate(() => {
+    //     let moreLinks = document.querySelectorAll('.row.anchorList table tbody tr td:first-child a');
+    //     return Array.from(moreLinks).map((link) => {return link.href});
+    // });
+
+    //Short way
+    //@Arr of Expansion Name and link to the cards
+    let expansionLinks = await page.$$eval(linksSelector,  (el) => el.map((link) => { return [link.innerHTML,link.href]}));
+
+
+    for (let i = 0; i < 5; i++) {
+        const newTab = await browser.newPage();
+        console.log(expansionLinks[i][1]);
+        await newTab.goto(expansionLinks[i][1]);
+        console.log("hola");
+        await newTab.close();
+    }
+
+
+
+    await browser.close();
+})();
